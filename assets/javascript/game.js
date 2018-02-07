738f759311b82287edb6943b13b8f93fb5cc193d
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-07 12:26:22 -0800

    closer to working, finding the letter in string, but wont return that letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,73 @@
 $(document).ready(function(){
     var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     var lettersLength = letters.length
-    var shows = ["RUGRATS", "DOUG", "DARIA", "POKEMON", "RECESS", "ANAMANIACS", "CARMEN/SAN/DIEGO", "REN AND STIMPY", "THE SIMPSONS", "POWER RANGERS", "BEAVIS AND BUTTHEAD", "ROCKOS MODERN LIFE", "NINJA TURTLES"]
-    var showsLength = shows.length
-    var randomShow = shows[Math.floor(Math.random() * showsLength)]
+    var words  = ["RUGRATS", "DOUG", "DARIA", "POKEMON", "RECESS", "ANAMANIACS", "CARMEN/SAN/DIEGO", "REN AND STIMPY", "THE SIMPSONS", "POWER RANGERS", "BEAVIS AND BUTTHEAD", "ROCKOS MODERN LIFE", "NINJA TURTLES"]
+    var randomWord = words[Math.floor(Math.random() * words.length)]
     var answer = [];
-    var wins = 0
+    var wins = "0"
     $(".wins").text("WINS : " + wins)
     var losses = 0
     $(".losses").text("LOSSES : " + losses)
-    var lives = 6
+    var guessesLeft = []
     var hints = ["Gang of Babies", "Quail man's identity", "90's femenist", "Gotta Catch 'em all!", "playtime during school, not lunch", "They live in the WB tower", "Where in the world is...", "'It's log, log...", "Longest running cartoon.", "Morphin' time!", "'I need teepee for my bunghole'", "Friendly walabe", "Pizza lovin' turtles" ]
     var hintLength = hints.length
-    var positions = []
     var getHint = $("#hintBtn")
     $(".hintBtn").on("click", function(){
-        if(shows[0] === randomShow) {
+        if(words[0] === randomWord) {
             $("#hint").text(hints[0])
         }
-        else if (shows[1]=== randomShow) {
+        else if (words[1]=== randomWord) {
             $("#hint").text(hints[1])
         }
-        else if (shows[2]=== randomShow) {
+        else if (words[2]=== randomWord) {
             $("#hint").text(hints[2])
         }
-        else if (shows[3]=== randomShow) {
+        else if (words[3]=== randomWord) {
             $("#hint").text(hints[3])
         }
-        else if (shows[4]=== randomShow) {
+        else if (words[4]=== randomWord) {
             $("#hint").text(hints[4])
         }
-        else if (shows[5]=== randomShow) {
+        else if (words[5]=== randomWord) {
             $("#hint").text(hints[5])
         }
-        else if (shows[6]=== randomShow) {
+        else if (words[6]=== randomWord) {
             $("#hint").text(hints[6])
         }
-        else if (shows[7]=== randomShow) {
+        else if (words[7]=== randomWord) {
             $("#hint").text(hints[7])
         }
-        else if (shows[8]=== randomShow) {
+        else if (words[8]=== randomWord) {
             $("#hint").text(hints[8])
         }
-        else if (shows[9]=== randomShow) {
+        else if (words[9]=== randomWord) {
             $("#hint").text(hints[9])
         }
-        else if (shows[10]=== randomShow) {
+        else if (words[10]=== randomWord) {
             $("#hint").text(hints[10])
         }
-        else if (shows[11]=== randomShow) {
+        else if (words[11]=== randomWord) {
             $("#hint").text(hints[11])
         }
-        else if (shows[12]=== randomShow) {
+        else if (words[12]=== randomWord) {
             $("#hint").text(hints[12])
         }
     })
-var wrongLetter = []
-var myGuess = []
+    var wrongLetter = []
+
 
     //generates random word on click
-    $("#newGame").on("click", function(){
-        randomShow = (shows[Math.floor(Math.random() * showsLength)])
+    $("#newGame").on("click", function(event){
+        randomWord = (words[Math.floor(Math.random() * words.length)])
         answer = []
-        for (var i = 0; i < randomShow.length; i++) {
-            answer[i] = answer + "_ ";
+        for (var i = 0; i < randomWord.length; i++) {
+            answer[i] =  "_ ";
         }
-        // $(".display").html(answer.join(" "))
+        $(".display").html(answer.join(" "))
         $("#hint").html("Press the button to get a hint!")
-        $(".lives").text("You have " + lives + " guesses left!");
-        console.log(randomShow)
+        guessesLeft = 10
+        $(".guessesLeft").text("You have " + guessesLeft + " guesses left!");
+        console.log(randomWord)
     })
 
     // create buttons for letters
@@ -85,40 +84,31 @@ var myGuess = []
         var letterBox = $("<span>")
         letterBox.attr("class", "letter")
         letterBox.text($(this).attr("data-letter"))
-        $("#display").append(letterBox)
+        $(".display").append(letterBox)
 
         //gets value for click on virtual keyboard
         for (var i = 0; i < letters.length; i++) {
             $(".letter").on("click", function(){
-                var input = $(".letter").val();
-                console.log(input)
+                var myGuess = $(".letter").val();
+                console.log(letters[i])
+                console.log(myGuess)
 
                 //logs the connection
-                var myGuess = input
-                if(randomShow.indexOf(myGuess)> -1) {
-                    for(var j = 0; j < randomShow.length; j++) {
-                        if(randomShow[j] === myGuess) {
-                            
-                            console.log("great job!")
-                        }
-                    }
+                if(randomWord.indexOf(myGuess) > -1) {
+                    console.log("hello")
+                    wins++
+                }
+            });
 
-                    function getAllIndexes (word,val) {
-                        pos = str.indexOf (randomShow[i]);
-                        for (i = 0; i < randomShow.length; i++) {
-                            if(randomShow[i]===attr("data-letter")) {
-                                positions.push(i)
-                            }
-                            if (randomShow[i]===input) {
-                                ///push to space in #show
-                            }
-                        }
-                    }
-                   
+            function getAllIndexes (word, myGuess) {
+                var positions = randomWord.indexOf(myGuess)
+                for (i = 0; i < randomWord.length; i++) {
+                    if(randomWord[i]===myGuess)
+                    console.log("yay")
                 }
 
-            });
-  
+            }
+            
         }
     })
 })
--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,67 +1,104 @@
+
 $(document).ready(function(){
-    var shows = ["POKEMON", "SABRINA", "DOUG", "HEY ARNOLD", "NINJA TURTLES", "YUGIOH"]
-    var show = shows[Math.floor(Math.random() * shows.length)]
-    var letters = show.split("")
-    var guesses = 10
-    $(".guessesLeft").text("You have " + guesses + " guesses left!");
-    var userLives = []
-    $(".lives")
-    var remainingLetters = letters.length
-    var userGuess = []
-    var answerArray = []
-    for (var i = 0; i < letters.length; i++)
-      answerArray[i] = "_ "
 
-    $("#newGame").on("click", function(){
-        show = shows[Math.floor(Math.random() * shows.length)]
-        //displays word to guess as underscores
-        guesses = 10
-        var answerArray = []
-        for (var i = 0; i < letters.length; i++)
-          answerArray.push("_")
-        $(".display").html(answerArray.join(" "))
-        //resets lives on button click
-        console.log(letters)
-        console.log(answerArray)
-        console.log(letters[2])
-        console.log(guesses)
-    })
-
-    //gets letter on keyboard letter press
-    $(document).on("keypress", function(){
-        userGuess = String.fromCharCode(event.keyCode).toUpperCase()
-        
-        // console.log(userGuess)
-
-        //while loop
-        if (guesses > 0) {
-            for (var j = 0; j < letters.length; j++) {
-                // console.log(show[j])
-                // console.log(show[2])
-                if (letters[j] === userGuess) { 
-                    answerArray.push(userGuess)
-                    console.log(answerArray)
-                    remainingLetters--
-                    console.log(letters[j])
-                }
-
-                else if(letters[j] != userGuess) {
-                    guesses--;
-                    $(".guesses").append(userGuess + " ")
-                    console.log(letters[j])
-        
-                }
-//push wrong guesses
-                if (guesses == 0) {
-                    alert("YOU LOST!")
-                    $(document).on("keypress", function() {
-                        null
-                    })
-                }
-                   
-            }
-            
-        
+    var words  = ["RUGRATS", "DOUG", "DARIA", "POKEMON", "RECESS", "ANAMANIACS", "CARMEN/SAN/DIEGO", "REN AND STIMPY", "THE SIMPSONS", "POWER RANGERS", "BEAVIS AND BUTTHEAD", "ROCKOS MODERN LIFE", "NINJA TURTLES", "MATILDA"]
+    var chosenWord = words[Math.floor(Math.random()* words.length)]
+    // var space = ''
+    var underScores = [] //underscores
+    var wrongGuesses = [] //store wrong guesses
+    var guesses = 10 //guess max
+    //replace 
+    var wins= 0;
+    $(".wins").html("WINS : " + wins)
+    var hints = ["Gang of Babies", "Quail man's identity", "90's femenist", "Gotta Catch 'em all!", "playtime during school, not lunch", "They live in the WB tower", "Where in the world is...", "'It's log, log...", "Longest running cartoon.", "Morphin' time!", "'I need teepee for my bunghole'", "Friendly walabe", "Pizza lovin' turtles", "Girl Genius"]
+    var hintLength = hints.length
+    var getHint = $("#hintBtn")
+    $(".hintBtn").on("click", function(){
+        if(words[0] === chosenWord) {
+            $("#hint").text(hints[0])
+        }
+        else if (words[1]=== chosenWord) {
+            $("#hint").text(hints[1])
+        }
+        else if (words[2]=== chosenWord) {
+            $("#hint").text(hints[2])
+        }
+        else if (words[3]=== chosenWord) {
+            $("#hint").text(hints[3])
+        }
+        else if (words[4]=== chosenWord) {
+            $("#hint").text(hints[4])
+        }
+        else if (words[5]=== chosenWord) {
+            $("#hint").text(hints[5])
+        }
+        else if (words[6]=== chosenWord) {
+            $("#hint").text(hints[6])
+        }
+        else if (words[7]=== chosenWord) {
+            $("#hint").text(hints[7])
+        }
+        else if (words[8]=== chosenWord) {
+            $("#hint").text(hints[8])
+        }
+        else if (words[9]=== chosenWord) {
+            $("#hint").text(hints[9])
+        }
+        else if (words[10]=== chosenWord) {
+            $("#hint").text(hints[10])
+        }
+        else if (words[11]=== chosenWord) {
+            $("#hint").text(hints[11])
+        }
+        else if (words[12]=== chosenWord) {
+            $("#hint").text(hints[12])
+        }
+        else if (words[13]=== chosenWord) {
+            $("#hint").text(hints[12])
         }
     })
+    
+    $("#newGame").on("click", function(){
+        chosenWord = words[Math.floor(Math.random() * words.length)]
+        underScores = []
+        for (var i = 0; i < chosenWord.length; i++) {
+            underScores.push("_ ")
+        }
+        guesses = 10
+        $(".guessesLeft").html("You have " + guesses + " guesses left!")
+        $(".display").html(underScores)
+        $("#hint").html("Press the button to get a hint!")
+        console.log("your random word is " + chosenWord)
+    })
+    
+    if (guesses > 0) { 
+        $(document).on("keyup", function(event){
+            guess = String.fromCharCode(event.keyCode).toUpperCase()
+            // console.log(guess)
+            if (chosenWord.indexOf(guess) > -1) {
+                for (i = 0; i < chosenWord.length; i++) {
+                    if ( chosenWord[i] = guess) {
+                        underScores[i] = chosenWord[i]
+                        console.log(underScores)
+                        underScores.join(" ")
+                        return
+                    }  
+                }
+            }
+            
+            else {
+                for (i = 0; i < chosenWord.length; i++) {
+                    if ( chosenWord[i] != guess) {
+                        guesses--
+                    var wrongGuesses = guess
+                    $(".wrongGuesses").append(wrongGuesses)
+                    console.log(wrongGuesses)
+                    return
+                    }    
+                    
+                }
+            }
+        })        
+    }
+
 })
