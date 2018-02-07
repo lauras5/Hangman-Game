76fcc2f1884b2bad22c8fb00f2361da5f481a519
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-07 15:40:15 -0800

    need to find all indexes, not just first index, write function

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -2,17 +2,19 @@
 $(document).ready(function(){
 
     var words  = ["RUGRATS", "DOUG", "DARIA", "POKEMON", "RECESS", "ANAMANIACS", "CARMEN/SAN/DIEGO", "REN AND STIMPY", "THE SIMPSONS", "POWER RANGERS", "BEAVIS AND BUTTHEAD", "ROCKOS MODERN LIFE", "NINJA TURTLES", "MATILDA"]
+    //figure out how to add spaces
+    //need function for finding all occurances, not just first
     var chosenWord = words[Math.floor(Math.random()* words.length)]
-    // var space = ''
     var underScores = [] //underscores
     var wrongGuesses = [] //store wrong guesses
     var guesses = 10 //guess max
-    //replace 
     var wins= 0;
     $(".wins").html("WINS : " + wins)
     var hints = ["Gang of Babies", "Quail man's identity", "90's femenist", "Gotta Catch 'em all!", "playtime during school, not lunch", "They live in the WB tower", "Where in the world is...", "'It's log, log...", "Longest running cartoon.", "Morphin' time!", "'I need teepee for my bunghole'", "Friendly walabe", "Pizza lovin' turtles", "Girl Genius"]
     var hintLength = hints.length
     var getHint = $("#hintBtn")
+
+    //hint button onclick function
     $(".hintBtn").on("click", function(){
         if(words[0] === chosenWord) {
             $("#hint").text(hints[0])
@@ -57,31 +59,41 @@ $(document).ready(function(){
             $("#hint").text(hints[12])
         }
     })
-    
-    $("#newGame").on("click", function(){
+
+    //new game function is acting as reset
+    function reset() {
         chosenWord = words[Math.floor(Math.random() * words.length)]
         underScores = []
         for (var i = 0; i < chosenWord.length; i++) {
             underScores.push("_ ")
         }
         guesses = 10
+        $(".wrongGuesses").html(' ')
         $(".guessesLeft").html("You have " + guesses + " guesses left!")
         $(".display").html(underScores)
         $("#hint").html("Press the button to get a hint!")
         console.log("your random word is " + chosenWord)
+    }
+
+    $("#newGame").on("click", function(){
+        reset()
     })
-    
+
+
     if (guesses > 0) { 
-        $(document).on("keyup", function(event){
-            guess = String.fromCharCode(event.keyCode).toUpperCase()
+        $(document).on("keyup", function(e){
+            guess = String.fromCharCode(e.keyCode).toUpperCase()
+            if (e.which <= 90 && e.which >= 48)
             // console.log(guess)
             if (chosenWord.indexOf(guess) > -1) {
                 for (i = 0; i < chosenWord.length; i++) {
-                    if ( chosenWord[i] = guess) {
+                    if ( chosenWord[i] == guess) {
                         underScores[i] = chosenWord[i]
-                        console.log(underScores)
+                        // console.log(underScores)
                         underScores.join(" ")
+                        $(".display").html(underScores)
                         return
+                        
                     }  
                 }
             }
@@ -90,15 +102,27 @@ $(document).ready(function(){
                 for (i = 0; i < chosenWord.length; i++) {
                     if ( chosenWord[i] != guess) {
                         guesses--
-                    var wrongGuesses = guess
-                    $(".wrongGuesses").append(wrongGuesses)
-                    console.log(wrongGuesses)
+                        $(".guessesLeft").html("You have " + guesses + " guesses left!")
+                        var wrongGuesses = guess
+                        $(".wrongGuesses").append(wrongGuesses)
+                        if ( guesses === 0) {               
+                            alert("You Lost! The answer was " + chosenWord + " ! Play again!!")
+                            reset()
+                        }
+
+                    
+                    // console.log(wrongGuesses)
                     return
+            
                     }    
                     
                 }
             }
+
+            
         })        
     }
+
+
 
 })
